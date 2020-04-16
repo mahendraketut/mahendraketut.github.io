e774f8059af82ebbaa4831e112e702a519a57a9a
mketut18 <63593289+mketut18@users.noreply.github.com>
2020-04-16 22:24:15 +0800

    initial first commit

---
+++ javascript/loader.js
@@ -0,0 +1,12 @@
+//JavaScript Document
+
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 50);
+}
+
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
+}
---
+++ jquery/alert-contact.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("form").submit(function() {
+        alert("Your Data Succesfuly Submitted! Thank You");
+        alert("For more Information, Please Contact +62361 225100")
+    });
+});
